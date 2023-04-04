46c607f55ace7a93b94958c87e8b76a6b95f37aa
Ben Cartwright-Cox <ben@benjojo.co.uk>
2023-04-04 22:35:51 +0100

    Add Video support
    Mild-reformatting-by: Zev Weiss <zev@bewilderbeest.net>

--- web.go
+++ web.go
@@ -229,6 +229,9 @@ func showrss(w http.ResponseWriter, r *http.Request) {
 				d.URL, d.Desc))
 			if strings.HasPrefix(d.Media, "image") {
 				desc += string(templates.Sprintf(`<img src="%s">`, d.URL))
+			} else if strings.HasPrefix(d.Media, "video") {
+				s := `<video loop="true" autoplay="true" muted="true" style="max-width: 100%;height: auto;"><source src="%s" type="video/mp4"></video>`;
+				desc += string(templates.Sprintf(s, d.URL))
 			}
 		}
 
